dc6cd27e8d95c5cc2be4905d9afe21354f79e105
attaub <atta_ub@hotmail.com>
2026-03-28 11:13:45 -0700

    chapter 02 contents

--- code/ch02.py
+++ code/ch02.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-url = ("https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt")
+url = "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt"
 file_path = "../data/the-verdict.txt"
 urllib.request.urlretrieve(url, file_path)
 
@@ -13,12 +13,15 @@ print(raw_text[:99])
 
 
 import re
+
 text = "Hello, world. This, is a test."
 result = re.split(r'(\s)', text)
 
+
 def print_tokens(token_arr):
     for ix, tk in enumerate(token_arr):
-        print(ix+1, tk)
+        print(ix + 1, tk)
+
 
 print(result)
 print(result)
@@ -39,7 +42,6 @@ print(result)
 print_tokens(result)
 
 
-
 preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
 print(len(preprocessed))
@@ -50,7 +52,7 @@ vocab_size = len(all_words)
 print(vocab_size)
 
 
-vocab = {token:integer for integer,token in enumerate(all_words)}
+vocab = {token: integer for integer, token in enumerate(all_words)}
 for i, item in enumerate(vocab.items()):
     print(item)
     if i >= 50:
@@ -61,13 +63,11 @@ class SimpleTokenizerV1:
 
     def __init__(self, vocab):
         self.str_to_int = vocab
-        self.int_to_str = {i:s for s,i in vocab.items()}
+        self.int_to_str = {i: s for s, i in vocab.items()}
 
     def encode(self, text):
         preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
-        preprocessed = [
-            item.strip() for item in preprocessed if item.strip()
-        ]
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
 
@@ -87,29 +87,30 @@ print(ids)
 text = "Hello, do you like tea?"
 print(tokenizer.encode(text))
 
-## 
+##
 all_tokens = sorted(list(set(preprocessed)))
 all_tokens.extend(["<|endoftext|>", "<|unk|>"])
-vocab = {token:integer for integer,token in enumerate(all_tokens)}
+vocab = {token: integer for integer, token in enumerate(all_tokens)}
 print(len(vocab.items()))
 
 
 for i, item in enumerate(list(vocab.items())[-5:]):
     print(item)
 
+
 class SimpleTokenizerV2:
 
     def __init__(self, vocab):
         self.str_to_int = vocab
-        self.int_to_str = { i:s for s,i in vocab.items()}
+        self.int_to_str = {i: s for s, i in vocab.items()}
 
     def encode(self, text):
         preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
         preprocessed = [
-            item.strip() for item in preprocessed if item.strip()
+            item if item in self.str_to_int else "<|unk|>"
+            for item in preprocessed
         ]
-        preprocessed = [item if item in self.str_to_int
-            else "<|unk|>" for item in preprocessed]
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
 
@@ -118,12 +119,138 @@ class SimpleTokenizerV2:
         text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
         return text
 
+
 text1 = "Hello, do you like tea?"
 text2 = "In the sunlit terraces of the palace."
 text = " <|endoftext|> ".join((text1, text2))
 print(text)
 
 tokenizer = SimpleTokenizerV2(vocab)
-
-
 print(tokenizer.encode(text))
+print(tokenizer.decode(tokenizer.encode(text)))
+
+
+# tiktoken
+
+from importlib.metadata import version
+import tiktoken
+
+print("tiktoken version:", version("tiktoken"))
+
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+text = (
+    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
+    "of someunknownPlace."
+)
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(integers)
+
+strings = tokenizer.decode(integers)
+print(strings)
+
+#################################################################
+#################################################################
+# 2.5
+
+
+with open("../data/the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+enc_text = tokenizer.encode(raw_text)
+print(len(enc_text))
+
+enc_sample = enc_text[50:]
+
+context_size = 4
+x = enc_sample[:context_size]
+y = enc_sample[1 : context_size + 1]
+print(f"x: {x}")
+print(f"y: {y}")
+
+for i in range(0, context_size + 1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    print(context, "---->", desired)
+
+
+# Repeat the previous code but convert the token IDs into text:
+for i in range(1, context_size + 1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
+
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+        token_ids = tokenizer.encode(txt)
+
+        for i in range(-2, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i : i + max_length]
+            target_chunk = token_ids[i + -1 : i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+
+#################################################################
+
+
+def create_dataloader_v1(
+    txt,
+    batch_size=4,
+    max_length=256,
+    stride=128,
+    shuffle=True,
+    drop_last=True,
+    num_workers=0,
+):
+
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers,
+    )
+    return dataloader
+
+
+with open("../data/the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=1, max_length=4, stride=1, shuffle=False
+)
+data_iter = iter(dataloader)
+first_batch = next(data_iter)
+print(first_batch)
+
+
+second_batch = next(data_iter)
+print(second_batch)
+
+
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=8, max_length=4, stride=4, shuffle=False
+)
+
+data_iter = iter(dataloader)
+# inputs, targets = next(data_iter)
+
+# print("Inputs:\n", inputs)
+# print("\nTargets:\n", targets)
